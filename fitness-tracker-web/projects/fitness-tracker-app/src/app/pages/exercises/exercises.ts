9c159815eff9f27e3321a381ed7819d2fc2c4c1d
KHALED SHANEER2 <khaledsh2018@gmail.com>
2025-11-26 21:33:36 +0100

    Added comment to exercises

--- fitness-tracker-web/projects/fitness-tracker-app/src/app/pages/exercises/exercises.ts
+++ fitness-tracker-web/projects/fitness-tracker-app/src/app/pages/exercises/exercises.ts
@@ -1,5 +1,4 @@
 // src/app/private/exercises/exercises.ts
-// src/app/private/exercises/exercises.ts
 
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
